9d14b012b8c21ad941c4ee855c7516c3b6606901
Rubén Fresneda <cem20814@gmail.com>
2024-06-18 20:14:10 +0200

    initial commit

---
+++ src/main/java/com/keepcoding/intro/initializr/hello_spring_initializr/HelloSpringInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.keepcoding.intro.initializr.hello_spring_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keepcoding/intro/initializr/hello_spring_initializr/HelloSpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepcoding.intro.initializr.hello_spring_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
